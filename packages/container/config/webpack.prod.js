08e2fdf5f09723db48d6a72f35b8f4b3457731b2
moryno <mauricenganga41@gmail.com>
2024-06-12 10:44:09 +0300

    Add public path in the output of the webpack

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,22 +3,23 @@ const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
-const domain = process.env.PRODUCTION_DOMAIN
+const domain = process.env.PRODUCTION_DOMAIN;
 
 const productionConfig = {
-    mode: "production",
-    output: {
-        filename: "[name].[contenthash].js"
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: "container",
-            filename: "remoteEntry.js",
-            remotes:{
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`
-            },
-            shared: packageJson.dependencies
-        })
-    ]
-}
-module.exports = merge(commonConfig, productionConfig)
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      filename: "remoteEntry.js",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+module.exports = merge(commonConfig, productionConfig);
