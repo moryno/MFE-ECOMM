fa929bacb0b0b2a0494278f2e3caf5d31a42bf7f
moryno <mauricenganga41@gmail.com>
2024-06-12 07:57:10 +0300

    Add routing import to the app file

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-    return <MarketingApp />
-}
+  return <MarketingApp />;
+};
