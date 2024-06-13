76bdb3907bfbb8456cabf1b3b4bd155e6e7ce6cd
moryno <mauricenganga41@gmail.com>
2024-06-13 08:14:08 +0300

    Modify styles

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
-  return <MarketingApp />;
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <Router>
+        <Header />
+        <MarketingApp />
+      </Router>
+    </StylesProvider>
+  );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,20 +1,22 @@
-import React from "react"
-import { Switch, Route, BrowserRouter as Router} from "react-router-dom"
-import { StylesProvider } from "@material-ui/core"
-import Landing from './components/Landing'
-import Pricing from './components/Pricing'
+import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
 
 export default () => {
-    return (
-        <div>
-            <StylesProvider>
-              <Router>
-                <Switch>
-                    <Route exact path="/pricing" component={Pricing} />
-                    <Route path="/" component={Landing} />
-                </Switch>
-              </Router>
-            </StylesProvider>
-        </div>
-    )
-}
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <Router>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </Router>
+    </StylesProvider>
+  );
+};
